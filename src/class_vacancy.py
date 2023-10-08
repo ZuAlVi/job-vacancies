f026072a9e136ed00e0fc4885f0c9067c0b86f09
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-08 20:20:32 +0300

    Added get_info method in class_vacancy.py

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -14,3 +14,21 @@ class Vacancy:
         self.employer = vacancy_data['employer']
 
         Vacancy.all_vacancies.append(self)
+
+    def get_info(self) -> None:
+        """Метод выводит информацию об экземпляре класса в удобном виде."""
+        print('~' * 118)
+        print(f'Работодатель: {self.employer[:100]}')
+        print(f'Профессия - {self.name}')
+        if self.requirement is None:
+            print('Требования не указаны.')
+        else:
+            print(f'Требования - {self.requirement[:100]}...')
+        if self.responsibility is None:
+            print('Сфера деятельности не указана.')
+        else:
+            print(f'Сфера деятельности - {self.responsibility[:100]}...')
+        print(f'Зарплатные ожидания: {self.salary} {self.salary_currency}.')
+        print(f'Ссылка на вакансию - {self.url}')
+        print('~' * 118)
+        print()
