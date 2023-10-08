8f496a24eb97ef18ea93ccb924549c08047c2e32
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-08 18:08:28 +0300

    Created class HeadHunterAPI in class_hh_api.py

---
+++ src/class_hh_api.py
@@ -0,0 +1,17 @@
+from src.abstract_classes import API
+
+
+class HeadHunterAPI(API):
+    """Класс для получения информации из API сайта headhunter.ru """
+
+    def __init__(self):
+        pass
+
+    def get_vacancies(self):
+        pass
+
+    def add_profession(self, value):
+        pass
+
+    def format_data(self, data):
+        pass
