3b72d1569b44b318208dabd1d5ec270ae4f96dba
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-08 18:55:29 +0300

    Added __init__ method body in class_vacancy.py

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -1,2 +1,16 @@
 class Vacancy:
     """Класс для работы с вакансиями."""
+
+    all_vacancies = []
+    request_text = ''
+
+    def __init__(self, vacancy_data: dict):
+        self.name = vacancy_data['name']
+        self.requirement = vacancy_data['requirement']
+        self.responsibility = vacancy_data['responsibility']
+        self.salary = vacancy_data['salary']
+        self.salary_currency = vacancy_data['salary_currency']
+        self.url = vacancy_data['url']
+        self.employer = vacancy_data['employer']
+
+        Vacancy.all_vacancies.append(self)
