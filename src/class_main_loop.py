1e134812e7a4a8492a4865e16a351ab112660413
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-08 20:26:10 +0300

    Added __init__ method in class_main_loop.py

--- src/class_main_loop.py
+++ src/class_main_loop.py
@@ -1,3 +1,12 @@
+from src.class_hh_api import HeadHunterAPI
+from src.class_sj_api import SuperJobAPI
+
+
 class MainLoop:
     """Этот класс предназначен для взаимодействия
      с пользователем в консоли."""
+
+    def __init__(self):
+        self.hh_api = HeadHunterAPI()
+        self.sj_api = SuperJobAPI()
+
