e5d85329fe0ff9be6759f1a5beee7c47b5841e2f
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-08 18:44:02 +0300

    Added __init__ method body in class_hh_api.py

--- src/class_hh_api.py
+++ src/class_hh_api.py
@@ -1,11 +1,22 @@
+import copy
+
 from src.abstract_classes import API
 
 
 class HeadHunterAPI(API):
     """Класс для получения информации из API сайта headhunter.ru """
 
+    __HH_API_URL = 'https://api.hh.ru/vacancies'
+
+    param_default = {
+        'per_page': 50,
+        'area': 1,
+        'date': 7,
+        'only_with_salary': True
+    }
+
     def __init__(self):
-        pass
+        self.param = copy.deepcopy(self.param_default)
 
     def get_vacancies(self):
         pass
