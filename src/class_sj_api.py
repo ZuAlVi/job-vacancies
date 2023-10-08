95237aff76a9c7773f56cbfbf67005a153b35fd2
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-08 20:14:40 +0300

    Added __init__ method body in class_sj_api.py

--- src/class_sj_api.py
+++ src/class_sj_api.py
@@ -1,11 +1,23 @@
+import copy
+import os
+
 from src.abstract_classes import API
 
 
 class SuperJobAPI(API):
     """Класс для получения информации из API сайта superjob.ru """
 
+    __SJ_API_URL = 'https://api.superjob.ru/2.0/vacancies/'
+    __SJ_API_KEY = os.getenv('SECRET_KEY')
+
+    param_default = {
+        'count': 50,
+        'town': 'Москва',
+        'period': 7,
+    }
+
     def __init__(self):
-        pass
+        self.param = copy.deepcopy(self.param_default)
 
     def get_vacancies(self):
         pass
